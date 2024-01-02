42509fea59d31cb114f87a0f0bde0902649465f4
Mike <mike@graywind.org>
2024-01-01 19:38:31 -0600

    feat: testing and cleanup (#14)
    * feat: testing and cleanup
    
    features
    - significant unit test coverage
    - some refactors to accommodate testing
    - e2e tests
    - GHA workflows to do all testing and linting
    - bandit
    - safety
    - flake8
    - isort
    - ruff
    - mypy
    
    fixes for ci/cd
    
    more fixes
    
    test fixes
    
    more test fixes
    
    sudo
    
    more e2e
    
    more fixes
    
    further improvements
    
    improve e2e testing
    
    get better logs
    
    give it some time, and drop testing for mimic3
    
    get mimic running for e2e testing
    
    sudo it
    
    more e2e attempted fixes
    
    another approach for e2e fixes
    
    better logic
    
    more special sam work, force v1
    
    sam is very special
    
    more attempted fixes
    
    another approach
    
    branch
    
    piper tests
    
    * follow established plugin logging pattern
    
    remove now unnecessary mimic stuff
    
    * fix logging test

--- ovos_tts_plugin_server/__init__.py
+++ ovos_tts_plugin_server/__init__.py
@@ -1,24 +1,34 @@
-from typing import Optional
-import requests
+"""OpenVoiceOS companion plugin for OpenVoiceOS TTS Server."""
 import random
-from ovos_plugin_manager.templates.tts import TTS, TTSValidator, RemoteTTSException
+from typing import Any, Dict, List, Optional, Tuple
+
+import requests
+from ovos_plugin_manager.templates.tts import TTS, RemoteTTSException, TTSValidator
+from ovos_utils.log import LOG
+
+PUBLIC_TTS_SERVERS = [
+    "https://pipertts.ziggyai.online",
+    "https://tts.smartgic.io/piper",
+]
 
 
 class OVOSServerTTS(TTS):
-    public_servers = [
-        "https://pipertts.ziggyai.online",
-        "https://tts.smartgic.io/piper"
-    ]
+    """Interface to OVOS TTS server"""
+
+    public_servers: List[str] = PUBLIC_TTS_SERVERS
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs, audio_ext="wav",
-                         validator=OVOSServerTTSValidator(self))
+        super().__init__(*args, **kwargs, audio_ext="wav", validator=OVOSServerTTSValidator(self))
+        self.log = LOG
         if not self.verify_ssl:
-            self.log.warning("SSL verification disabled, this is not secure and should"
-                             "only be used for test systems! Please set up a valid certificate!")
+            self.log.warning(
+                "SSL verification disabled, this is not secure and should"
+                "only be used for test systems! Please set up a valid certificate!"
+            )
 
     @property
     def host(self) -> Optional[str]:
+        """If using a custom server, set the host here, otherwise it defaults to public servers."""
         return self.config.get("host", None)
 
     @property
@@ -28,55 +38,76 @@ class OVOSServerTTS(TTS):
 
     @property
     def verify_ssl(self) -> bool:
+        """Whether or not to verify SSL certificates when connecting to the server. Defaults to True."""
         return self.config.get("verify_ssl", True)
 
-    def get_tts(self, sentence, wav_file, lang=None, voice=None):
-        lang = lang or self.lang
-        voice = voice or self.voice
-        params = {"lang": lang, "voice": voice}
+    def get_tts(
+        self,
+        sentence,
+        wav_file,
+        lang: Optional[str] = None,
+        voice: Optional[str] = None,
+    ) -> Tuple[Any, None]:
+        """Fetch TTS audio using OVOS TTS server.
+        Language and voice can be overridden, otherwise defaults to config."""
+        params: Dict[str, Optional[str]] = {
+            "lang": lang or self.lang,
+            "voice": voice or self.voice,
+        }
         if not voice or voice == "default":
             params.pop("voice")
         if self.host:
             if isinstance(self.host, str):
-                servers = [self.host]
+                servers: List[str] = [self.host]
             else:
                 servers = self.host
         else:
-            servers = self.public_servers 
-        data = self._get_from_servers(params, sentence, servers)
-        with open(wav_file, "wb") as f:
-            f.write(data)
+            servers = self.public_servers
+        data: bytes = self._fetch_audio_data(params, sentence, servers)
+        self._write_audio_file(wav_file, data)
         return wav_file, None
 
-    def _get_from_servers(self, params: dict, sentence: str, servers: list):
+    def _write_audio_file(self, wav_file: str, data: bytes) -> None:
+        with open(file=wav_file, mode="wb") as f:
+            f.write(data)
+
+    def _fetch_audio_data(self, params: dict, sentence: str, servers: list) -> bytes:
+        """Get audio bytes from servers."""
         random.shuffle(servers)  # Spread the load among all public servers
         for url in servers:
             try:
                 if self.v2:
-                    url = f'{url}/v2/synthesize'
+                    url = f"{url}/v2/synthesize"
                     params["utterance"] = sentence
                 else:
-                    url = f'{url}/synthesize/{sentence}'
-                r = requests.get(url, params=params, verify=self.verify_ssl)
+                    url = f"{url}/synthesize/{sentence}"
+                r: requests.Response = requests.get(url=url, params=params, verify=self.verify_ssl, timeout=30)
                 if r.ok:
                     return r.content
-            except:
+                self.log.error(f"Failed to get audio, response from {url}: {r.text}")
+            except Exception as err:  # pylint: disable=broad-except
+                self.log.error(f"Failed to get audio from {url}: {err}")
                 continue
-        raise RemoteTTSException(f"All OVOS TTS servers are down!")
+        raise RemoteTTSException("All OVOS TTS servers are down!")
 
 
 class OVOSServerTTSValidator(TTSValidator):
-    def __init__(self, tts):
+    """Validate settings for OVOS TTS server plugin."""
+
+    def __init__(self, tts) -> None:  # pylint: disable=useless-parent-delegation
         super(OVOSServerTTSValidator, self).__init__(tts)
 
-    def validate_lang(self):
-        pass
+    def validate_lang(self) -> None:
+        """Validate language setting."""
+        return
 
-    def validate_connection(self):
-        pass
+    def validate_connection(self) -> None:
+        """Validate connection to server."""
+        return
 
     def get_tts_class(self):
+        """Return TTS class."""
         return OVOSServerTTS
 
 
-OVOSServerTTSConfig = {}
+OVOSServerTTSConfig: Dict[Any, Any] = {}
